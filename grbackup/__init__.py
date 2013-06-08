abecaced28d78088543c499e7ed657a2fcf143f5
wistful <wst.public.mail@gmail.com>
2013-06-09 00:33:53 +0300

    add license; first stable version.

--- grbackup/__init__.py
+++ grbackup/__init__.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-__version__ = '0.2'
+__version__ = '1.0'
 __description__ = 'Utilite to backup items from Google Reader'
 requires = [
     'pymongo>=2.4',
     'redis'
 ]
+README = """grbackup is a Python library used
+to save all items from your Google Reader account into different places.
+
+Usage
+=====
+
+::
+
+   list subscriptions: grbackup -e email@gmail.com -p password -ls
+   list topics: grbackup -e email@gmail.com -p password -lt http://feed.com
+   list starred: grbackup -e email@gmail.com -p password -lx
+   list all items: grbackup -e email@gmail.com -p password -la
+
+   backup subscriptions: grbackup -e email@gmail.com -p password -bs -o json:/tmp/subscriptions.json
+   backup topics: grbackup -e email@gmail.com -p password -bt http://myfeed.com -o json:/tmp/myfeed.json
+   backup starred into MongoDB: grbackup -e email@gmail.com -p password -bx -o mongodb://localhost:27017
+   backup all items into Redis: grbackup -e email@gmail.com -p password -ba -o redis://localhost:6379/3
+"""
--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 # coding=utf-8
+
 from setuptools import setup
-from grbackup import __version__, __description__, requires
+from grbackup import __version__, __description__, requires, README
 
 
 setup(name='grbackup',
       version=__version__,
       description=__description__,
       author='wistful',
+      author_email='wst.public.mail@gmail.com',
+      long_description=README,
+      url="https://bitbucket.org/wistful/grbackup",
+      license="MIT License",
       packages=['grbackup', 'grbackup.grb_plugins'],
       install_requires=requires,
       entry_points={
       'console_scripts': [
           'grbackup = grbackup.grbackup:entry_main'
       ]},
+      platforms=["Unix,"],
+      keywords="greader, backup",
+      test_suite='tests',
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 2 :: Only",
+          "Topic :: System :: Archiving :: Backup",
+          "Topic :: Utilities"
+      ],
       )
